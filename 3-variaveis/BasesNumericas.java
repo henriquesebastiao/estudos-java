c3a4ef69def0857cdc57121dfa255576aa30b51f
Henrique Sebastião <contato@henriquesebastiao.com>
2026-01-24 14:32:08 -0400

    Exemplo de como representar bases numéricas

---
+++ 3-variaveis/BasesNumericas.java
@@ -0,0 +1,8 @@
+public class BasesNumericas {
+  public static void main() {
+    int decVal = 26;
+    int hexVal = 0x1a;
+    int octVal = 032; // Números octais começam com 0
+    int binVal = 0b11010; // JDK 7
+  }
+}
