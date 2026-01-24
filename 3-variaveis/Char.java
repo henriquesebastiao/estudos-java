caf1b97568a4959a905996cc6f2f248c285a98e6
Henrique Sebastião <contato@henriquesebastiao.com>
2026-01-24 14:01:58 -0400

    Caracteres são números inteiros em Java

---
+++ 3-variaveis/Char.java
@@ -0,0 +1,9 @@
+public class Char {
+    public static void main() {
+        // Caracteres são números inteiros em Java
+        char o = 79; // Código decimal da letra O na tabela ASCII
+        char i = 105; // Código decimal da letra i na tabela ASCII
+
+        System.out.println("" + o + i);
+    }
+}
